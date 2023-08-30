98bf9f595dda0d339e961d882fb3327b2347ab79
Lalit Kumar <lalit.modi@datenwissen.com>
2023-08-30 23:29:03 +0530

    start command changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT =process.env.PORT||5000;
 const userRoutes = require("./Routes/users");
 const loginRoutes=require("./Routes/login")
 const GroupRoutes=require("./Routes/Group")
